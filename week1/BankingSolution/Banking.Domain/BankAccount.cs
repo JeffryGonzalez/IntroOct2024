70d25cca8f49af2942e3432fc294c30124e93929
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-16 12:36:52 -0400

    before lunch

--- week1/BankingSolution/Banking.Domain/BankAccount.cs
+++ week1/BankingSolution/Banking.Domain/BankAccount.cs
@@ -4,15 +4,14 @@ namespace Banking.Domain;
 
 public class BankAccount
 {
-    private decimal defaultBonus = 5000M;
+    private decimal _balance = 5000M;
     public void Deposit(decimal amountToDeposit)
     {
-        defaultBonus += amountToDeposit;
+        _balance += amountToDeposit;
     }
 
     public decimal GetBalance()
     {
-
-        return defaultBonus; // "slime"
+        return _balance; // "slime"
     }
 }
