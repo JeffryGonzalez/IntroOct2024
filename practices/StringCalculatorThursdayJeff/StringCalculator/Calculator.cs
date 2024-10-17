e88669f22f1e19c049994fe2070a70b95dbc7280
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-17 12:33:18 -0400

    before lunch

---
+++ practices/StringCalculatorThursdayJeff/StringCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+
+        if (numbers == "")
+        {
+            return 0;
+        }
+
+
+
+        // Language Integrated Query (LINQ)
+        return numbers.Split(',', '\n') // ["3","2"]
+            .Select(int.Parse) // [3, 2]
+            .Sum();
+
+
+
+    }
+}
