20d4834fa8791b28e74adab3091b685ea0d92045
Jeffry <jeff@hypertheory.com>
2024-10-23 08:30:14 -0400

    bank handlers

--- week2/frontend/src/app/banking/components/banking-transaction-input.component.ts
+++ week2/frontend/src/app/banking/components/banking-transaction-input.component.ts
@@ -3,16 +3,16 @@ import {
   ChangeDetectionStrategy,
   input,
   output,
-} from '@angular/core';
+} from "@angular/core";
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms';
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-banking-transaction-input',
+  selector: "app-banking-transaction-input",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [ReactiveFormsModule],
@@ -27,9 +27,12 @@ import {
       /><button type="submit" class="btn btn-primary">
         <label for="transaction-amount"> {{ label() }} </label>
       </button>
-      @if(form.invalid && (form.controls.transactionAmount.dirty ||
-      form.controls.transactionAmount.touched)) {
-      <span role="alert" class="alert alert-error">Errors</span>
+      @if (
+        form.invalid &&
+        (form.controls.transactionAmount.dirty ||
+          form.controls.transactionAmount.touched)
+      ) {
+        <span role="alert" class="alert alert-error">Errors</span>
       }
     </form>
   `,
--- week2/frontend/src/mocks/banking-handler.ts
+++ week2/frontend/src/mocks/banking-handler.ts
@@ -1,44 +1,58 @@
-import { TransactionRecord } from '../app/banking/types';
+import { HttpResponse, http } from "msw";
+import { TransactionRecord } from "../app/banking/types";
 
-const initialState: TransactionRecord[] = [
+const initialState: Omit<
+  TransactionRecord,
+  "startingBalance" | "newBalance"
+>[] = [
   {
-    id: 'd89d9641-4a24-47a1-9e1b-c45855a4ab0a',
+    id: "d89d9641-4a24-47a1-9e1b-c45855a4ab0a",
     amount: 562.23,
-    startingBalance: 0,
-    newBalance: 562.23,
-    type: 'deposit',
+    type: "deposit",
     created: 1729623072771,
   },
   {
-    id: '0f44ef39-16d5-41ff-8214-7d9e826669b7',
+    id: "0f44ef39-16d5-41ff-8214-7d9e826669b7",
     amount: 812.23,
-    startingBalance: 562.23,
-    newBalance: 1374.46,
-    type: 'deposit',
+    type: "deposit",
     created: 1729623077419,
   },
   {
-    id: '60358a6d-1c38-49df-ba05-48b77015a28f',
+    id: "60358a6d-1c38-49df-ba05-48b77015a28f",
     amount: 15.23,
-    startingBalance: 1374.46,
-    newBalance: 1359.23,
-    type: 'withdrawal',
+    type: "withdrawal",
     created: 1729623085091,
   },
   {
-    id: '684683ac-bb24-4f15-a35b-75462c50c90c',
+    id: "684683ac-bb24-4f15-a35b-75462c50c90c",
     amount: 19.23,
-    startingBalance: 1359.23,
-    newBalance: 1340,
-    type: 'withdrawal',
+    type: "withdrawal",
     created: 1729623089718,
   },
   {
-    id: '97b4ef70-75f4-4de8-b1b1-8dd75b73546e',
+    id: "97b4ef70-75f4-4de8-b1b1-8dd75b73546e",
     amount: 420.69,
-    startingBalance: 1340,
-    newBalance: 1760.69,
-    type: 'deposit',
+    type: "deposit",
     created: 1729623095591,
   },
 ];
+
+const handlers = [
+  http.get(
+    "http://fake-api.bankohypertheory.com/user/statements/:year/:month",
+    ({ params }) => {
+      const openingBalance = initialState
+        .map((a) => a.amount)
+        .reduce((a, b) => a + b, 0);
+      const response = {
+        accountNumber: "1234567890",
+        statementDate: `${params["year"]}-${params["month"]}`,
+        openingBalance,
+        transactions: initialState,
+      };
+      return HttpResponse.json(response);
+    },
+  ),
+];
+
+export default handlers;
