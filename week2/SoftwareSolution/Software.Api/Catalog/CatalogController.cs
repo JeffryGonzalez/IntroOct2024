d1f27fc91412f47e021324833176c0b4c1aed587
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-24 12:34:43 -0400

    before lunch

--- week2/SoftwareSolution/Software.Api/Catalog/CatalogController.cs
+++ week2/SoftwareSolution/Software.Api/Catalog/CatalogController.cs
@@ -5,7 +5,13 @@ namespace Software.Api.Catalog;
 public class CatalogController(CatalogManager catalogManager) : ControllerBase
 {
 
+    [HttpGet("/catalog")]
+    public async Task<ActionResult> GetFullCatalog(CancellationToken token)
+    {
 
+        IReadOnlyList<CatalogResponseModel> response = await catalogManager.GetCatalogAsync(token);
+        return Ok(response);
+    }
 
     [HttpPost("/catalog")]
     public async Task<ActionResult> AddSoftwareToCatalogAsync(
@@ -15,19 +21,9 @@ public class CatalogController(CatalogManager catalogManager) : ControllerBase
         {
             return BadRequest(ModelState); // 400
         }
-        // Controllers - validate the input.
 
-        // - decide what valid means, what should happen, not HOW it happens
-        // - decide what kind of response to return.
-        // Fake for a minute
-
-        // if the request is valid
-        // - save this thing into the database - return a 200 with the response model.
-        // if it is invalid
-        // - I'm going to return a 400 (optionally with some details)
 
         CatalogResponseModel response = await catalogManager.AddSoftwareToCatalogAsync(request);
-
 
 
         return Ok(response);
--- week2/SoftwareSolution/Software.Api/Catalog/CatalogManager.cs
+++ week2/SoftwareSolution/Software.Api/Catalog/CatalogManager.cs
@@ -3,10 +3,11 @@ using Marten;
 
 namespace Software.Api.Catalog;
 
-public class CatalogManager(IDocumentSession session)
+public class CatalogManager(IDocumentSession session, ILogger<CatalogManager> logger)
 {
     public async Task<CatalogResponseModel> AddSoftwareToCatalogAsync(CatalogCreateModel request)
     {
+        await Task.Delay(new Random().Next(1000, 3000));
 
         var response = new CatalogResponseModel()
         {
@@ -28,5 +29,24 @@ public class CatalogManager(IDocumentSession session)
         await session.SaveChangesAsync();
 
         return response;
+    }
+
+    public async Task<IReadOnlyList<CatalogResponseModel>> GetCatalogAsync(CancellationToken token)
+    {
+        logger.LogInformation("Starting a request to get the data from the database");
+        // Don't do this in production code.
+        await Task.Delay(2000, token);
+        var data = await session.Query<CatalogEntity>()
+            .Select(i => new CatalogResponseModel
+            {
+                Id = i.Id,
+                IsOpenSource = i.IsOpenSource,
+                Title = i.Title,
+                Vendor = i.Vendor,
+            })
+            .ToListAsync(token);
+
+        logger.LogInformation("Ending  the request to get the data from the database");
+        return data;
     }
 }
